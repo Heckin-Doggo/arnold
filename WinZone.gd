854d4dd569f659b9aa0653735fb465561bf1a3bd
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2019-09-07 12:18:50 -0400

    stuff from gdocs
    aaa

--- WinZone.gd
+++ WinZone.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 export var next_level = "Not_Set"
 onready var player = $"../Player"
+onready var globals = get_node("/root/Globals")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,4 +21,5 @@ func _process(delta):
 func _on_WinZone_body_entered(body):
 	if body.is_in_group("player"):
 		print("PLAYER REACHED END\n")
+		globals.player["lives"] += 1 # extra live per level
 		get_tree().change_scene(next_level)
