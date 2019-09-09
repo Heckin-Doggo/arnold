f019cd8ab8d9d25a310ba906285cd1b3a2fb00e4
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2019-09-08 21:00:16 -0400

    script fix

--- Police.gd
+++ Police.gd
@@ -1,8 +1,7 @@
 extends KinematicBody2D
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var walkspeed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
